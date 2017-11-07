6cce988311f98fe7fe56a3f3cc180f873ee755af
Konstantin Petryaev <numminorihsf@gmail.com>
2017-11-07 08:08:43 +0700

    Force rules for css modules

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 module.exports = {
   extends: [
     'stylelint-config-saritasa-base'
-  ]
+  ],
+  rules: {
+    'selector-max-specificity': ['0,2,1', {
+      ignoreSelectors: [':global']
+    }],
+    'selector-attribute-operator-whitelist': [],
+    'selector-max-class': 1,
+    'selector-max-id': 0,
+    'selector-max-type': 0,
+    'selector-max-universal': 0
+  }
 };
