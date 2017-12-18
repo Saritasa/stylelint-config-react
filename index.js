9e6b8b74b4f35f69d70786b99e9c0b42ee5656d3
Konstantin Petryaev <numminorihsf@gmail.com>
2017-12-18 07:34:07 +0700

    Update selector-class-pattern

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = {
     'selector-max-class': 1,
     'selector-max-id': 0,
     'selector-max-type': 0,
-    'selector-max-universal': 0
+    'selector-max-universal': 0,
+    'selector-class-pattern': [2, "^[a-z]+([A-Z][a-z]+)*$"]
   }
 };
